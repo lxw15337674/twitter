aa0e9179a83a6e0459ff0b341bb33aefdb1ee077
15515255978 <404174262@qq.com>
2020-02-11 15:18:00 +0800

    first

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,27 @@
     <div>
       <el-button>el-button</el-button>
     </div>
-    <el-image src="/img/logo.png" lazy :preview-src-list="[]" />
+    <div>
+      <el-image src="/img/logo.png" lazy :preview-src-list="[]" />
+    </div>
+    <el-button type="text" @click="dialogVisible = true"
+      >点击打开 Dialog</el-button
+    >
 
+    <el-dialog
+      title="提示"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :before-close="handleClose"
+    >
+      <span>这是一段信息</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogVisible = false"
+          >确 定</el-button
+        >
+      </span>
+    </el-dialog>
     <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
@@ -17,6 +36,20 @@ export default {
   name: "app",
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      dialogVisible: false
+    };
+  },
+  methods: {
+    handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
+    }
   }
 };
 </script>
